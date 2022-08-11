2c505d04b29ef43ec8b54b947574254d87208772
Seolang2 <dntmdqls0912@gmail.com>
2022-08-12 02:08:58 +0900

    Order list search with fetch join strategy and direct DTO search

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +29,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     // 단순히 배열을 return 할 시
     // Order 내의 Member에서 다시 Order를 참조하면서 무한루프에 빠진다.
@@ -66,6 +69,17 @@ public class OrderSimpleApiController {
         return orders.stream()
                 .map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+
+        // 쿼리를 직접 작성하므로써 select 절에서의 색인 데이터 양을 줄일 수 있다
+        // + 애플리케이션 네트워크 용량 최적화 (생각보다 미미)
+        // - Repository 재사용성이 낮아짐 (Repository는 Entity를 조회하기 위해 사용하는 것임)
+        // 차선책 : query용 repository를 따로 생성한다.
+
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -116,4 +116,5 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class
         ).getResultList();
     }
+
 }
