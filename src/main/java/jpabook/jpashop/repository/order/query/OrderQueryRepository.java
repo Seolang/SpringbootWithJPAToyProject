a20b3587e173e84edeef7fe2f9ca5a1fecc03a3a
Seolang2 <dntmdqls0912@gmail.com>
2022-08-16 23:09:27 +0900

    OneToMany DTO Collection Search strategy 2, "in" query and mapping

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,6 +15,27 @@ public class OrderQueryRepository {
     private final EntityManager em;
 
 
+    private List<OrderQueryDto> findOrders() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderQueryDto.class)
+                .getResultList();
+        // 쿼리상에서 DTO에 직접 담는 경우 fetch join이 필요하지 않다
+        // 왜냐하면 SQL상에서 데이터를 직접 가져와 바로 DTO로 넣기 떄문
+    }
+
+    private List<OrderItemQueryDto> findOrderItems(Long orderId) {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                                " from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id = :orderId", OrderItemQueryDto.class)
+                .setParameter("orderId", orderId)
+                .getResultList();
+    }
+
+    // 총 1+N번의 쿼리가 발생
     public List<OrderQueryDto> findOrderQueryDtos() {
         List<OrderQueryDto> result = findOrders();  // 쿼리 1번 -> N개
 
@@ -24,23 +47,37 @@ public class OrderQueryRepository {
         return result;
     }
 
-    private List<OrderItemQueryDto> findOrderItems(Long orderId) {
-        return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
-                        " from OrderItem oi" +
-                        " join oi.item i" +
-                        " where oi.order.id = :orderId", OrderItemQueryDto.class)
-                .setParameter("orderId", orderId)
-                .getResultList();
+    // 총 2번의 쿼리가 발생
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();  // 쿼리 1번
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+        result.forEach(o-> o.setOrderItems(orderItemMap.get(o.getOrderId())));  // 쿼리 1번
+
+        return result;
     }
 
-    private List<OrderQueryDto> findOrders() {
-        return em.createQuery(
-                        "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o" +
-                                " join o.member m" +
-                                " join o.delivery d", OrderQueryDto.class)
-                .getResultList();
-        // 쿼리상에서 DTO에 직접 담는 경우 fetch join이 필요하지 않다
-        // 왜냐하면 SQL상에서 데이터를 직접 가져와 바로 DTO로 넣기 떄문
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        List<Long> orderIds = result.stream()
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+        return orderIds;
     }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                                " from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id in :orderIds", OrderItemQueryDto.class) // 쿼리 1번
+                .setParameter("orderId", orderIds)
+                .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+        return orderItemMap;
+    }
+
+
 }
